9fa8cd8b028b4bf1ad305d1ce55f44cc49f7a527
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-11-06 09:18:44 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan35/tunjangan/oo/IF110119028Latihan35TunjanganOO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan35.tunjangan.oo;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan tunjangan
+ *                     saldo dengan konsep OOP
+ */
+public class IF110119028Latihan35TunjanganOO {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        // TODO code application logic here
+        Tunjangan gaji = new Tunjangan();
+        gaji.inputData();
+        gaji.cekStatus();
+        gaji.tampilFormat();
+    }
+    
+}
---
+++ src/if1/pkg10119028/latihan35/tunjangan/oo/Tunjangan.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan35.tunjangan.oo;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan tunjangan
+ *                     saldo dengan konsep OOP
+ */
+public class Tunjangan {
+    
+    public static String status;
+    public static Double gaji, tunjangan;
+    
+    public static void inputData() {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("==========Program Tunjangan==========");
+        System.out.print("Berapa gaji pokok anda perbulan?: Rp. ");
+        gaji = scanner.nextDouble();
+        
+        System.out.print("Status Anda?(Menikah/Belum)\t: ");
+        status = scanner.next();
+        while (!(status.toUpperCase().equals("MENIKAH") || status.toUpperCase().equals("BELUM"))) {
+            System.out.print("Status Anda?(Menikah/Belum)\t: ");
+                status = scanner.next();
+        }
+    }
+    
+    public static void cekStatus() {
+        tunjangan = (status.toUpperCase().equals("MENIKAH")) ? hitungTunjangan(gaji):0.0;
+    }
+    
+    public static double hitungTunjangan(double gaji) {
+        double hitungTunjangan = 0.35 * gaji;
+        return hitungTunjangan;
+    }
+    
+    public static void tampilFormat() {
+        System.out.println("=====Hasil Perhitungan Gaji Anda=====");
+        System.out.println("Gaji Pokok\t\t\t: Rp. " + gaji);
+        System.out.printf("Tunjangan\t\t\t: Rp. %.1f%n", tunjangan);
+        double totalGaji = gaji + tunjangan;
+        System.out.println("Total Gaji\t\t\t: Rp. " + totalGaji);
+        System.out.println("(Developed by : Maulana Imam Malik)");
+    }
+}
